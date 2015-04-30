ce6f207b6f8b4782df3015a32fc0d43512439749
Florenz Erstling <Florenz@F16.local>
2015-04-30 10:13:11 +0200

    jo

--- public/php/test/test.php
+++ public/php/test/test.php
@@ -1,4 +1,8 @@
 <?php
+require_once 'classDbFunctions.php';
+require_once 'classSessionHandler.php';
+require_once 'classStartTree.php';
+require_once 'classTrainerInfo.php';
 if ( $_REQUEST['action'] == 'login' ) {
 	echo "jojo";
 }
