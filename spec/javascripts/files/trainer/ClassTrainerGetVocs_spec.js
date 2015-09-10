25b55c193753d6ef9781e7a3ec8bb37c68a20f47
Florenz Erstling <florenz.erstling@gmx.de>
2015-09-10 16:03:33 +0200

    Try to understand the trainer

---
+++ spec/javascripts/files/trainer/ClassTrainerGetVocs_spec.js
@@ -0,0 +1,24 @@
+describe("ClassTrainerGetVocs***", function() {
+    var class_ajax = new ClassAjax();
+    var class_db_test_list = new ClassDbTestList();
+    var trainer = new ClassTrainer();
+
+    describe("db part", function() {
+        it("getEncodedArray should been defined", function() {
+            expect(trainer.getEncodedArray).toBeDefined();
+        });
+        it('check first voc', function() {
+            var voc_array = class_db_test_list.complete_array;
+            var expected = {
+                voc_id: voc_array[0].voc_id,
+                answer_id: [voc_array[0].answer_id, voc_array[1].answer_id, voc_array[2].answer_id],
+                rating: [voc_array[0].rating, voc_array[1].rating, voc_array[2].rating],
+            };
+            var list_id = voc_array[0].list_id;
+            var given = trainer.getEncodedArray(list_id);
+            expect(given[0].voc_id).toBe(expected.voc_id);
+            expect(given[0].answer_id).toEqual(expected.answer_id);
+            expect(given[0].rating).toEqual(expected.rating);
+        });
+    });
+});
---
+++ spec/javascripts/files/trainer/ClassTrainerUpdateRating_spec.js
@@ -0,0 +1,36 @@
+ddescribe("ClassTrainerUpdateVocRating***", function() {
+    var class_db_test_list = new ClassDbTestList();
+    var trainer = new ClassTrainer();
+    // todo split to calculateVocRating and updateVocRatingInDb
+    describe("db part", function() {
+        it("should work", function() {
+            var obj_array = class_db_test_list.user_data_array;
+            var voc_object = {
+                "voc_id": "2023",
+                "list_id": "9433",
+                "question": "a",
+                "answer": ["a","b"],
+                "answer_id": ["3238","2342"],
+                "right": ["3","4"],
+                "wrong": ["3","4"],
+                "rating": [-0.09,1],
+                "last_access": [1438812075,23423425],
+                "multi_choice": ["0","0"],
+                "img_id": ["0","0"],
+                "importance": [1,1],
+                "ok": [1,1]
+            };
+            trainer.poolnode = {
+                data : null
+            };
+            trainer.poolnode.data = voc_object;
+            var given_ratings = trainer.calculateRating();
+            console.log(given_ratings);
+            var expected_ratings = [];
+            expected_ratings[0] = parseInt(voc_object.rating[0], 10) + 1;
+            expected_ratings[1] = parseInt(voc_object.rating[1], 10) + 1;
+            expect(voc_object.rating[0]).toBe(expected_ratings[0]);
+            expect(voc_object.rating[1]).toBe(expected_ratings[1]);
+        });
+    });
+});
